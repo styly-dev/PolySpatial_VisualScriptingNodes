463222d9c06549217f81686f073262a2d5a3c280
afjk <afjk01@gmail.com>
2024-01-11 23:10:06 +0900

    Support rigidbody dragging.

--- PolySpatial_VisualScriptingNodes/Packages/com.styly.polyspatial-visualscripting-nodes/Runtime/Utils/Attr_Draggable.cs
+++ PolySpatial_VisualScriptingNodes/Packages/com.styly.polyspatial-visualscripting-nodes/Runtime/Utils/Attr_Draggable.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.PolySpatial;
 using UnityEngine;
 
@@ -8,15 +9,64 @@ namespace PolySpatialVisualScripting.Utils
     /// </summary>
     public class Attr_Draggable : MonoBehaviour
     {
+        private Rigidbody rigidbody;
+        private bool preIsKinematic;
+        private bool isDragging;
+        private Pose moveToPose;
+        
         void Start()
         {
             this.gameObject.AddComponent<PolySpatialHoverEffect>();
+            rigidbody = GetComponent<Rigidbody>();
         }
 
-        public void Select(bool selected, Vector3 interactionPosition)
+        public void Select(bool selected, Pose interactionPose)
         {
-            Debug.Log($"Select! :{selected} {interactionPosition}");
-            
+            Debug.Log($"Select! :{selected} {interactionPose}");
+            isDragging = selected;
+            moveToPose = interactionPose;
+        }
+      
+        private const float duration = 0.1f;
+
+        private void Update()
+        {
+            if (isDragging)
+            {
+                MoveTo(moveToPose);
+            }
+        }
+
+        public void MoveTo(Pose pose)
+        {
+            moveToPose = pose;
+            if (rigidbody && !rigidbody.isKinematic)
+            {
+                SetRigidbodyPosition(rigidbody, pose.position, duration);
+                SetRigidbodyRotation(rigidbody);
+            }
+            else
+            {
+                gameObject.transform.SetPositionAndRotation(pose.position, pose.rotation);
+            }
+        }
+        
+        void SetRigidbodyPosition(Rigidbody rb, Vector3 moveTo, float duration)
+        {
+            var diffPos = moveTo - gameObject.transform.position;
+            if (Mathf.Approximately(diffPos.sqrMagnitude, 0f))
+            {
+                rb.velocity = Vector3.zero;
+            }
+            else
+            {
+                rb.velocity = diffPos / duration;
+            }
+        }
+
+        void SetRigidbodyRotation(Rigidbody rb)
+        {
+            rb.angularVelocity = Vector3.zero;
         }
     }
 }
